2dada718de72dcc0d2e017f2b985d097c02f25e0
kowalix007 L.K <kowalix007@gmail.com>
2020-07-10 10:38:12 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,28 @@
+require 'pry'
+class Song 
+ 
+ attr_accessor :artist, :name
+   @@all = []
+ 
+  def initialize(name)
+     @name = name
+     save
+   end
+ 
+  def self.all
+     @@all
+   end
+ 
+  def save
+      @@all << self
+   end
+ 
+  def artist_name
+     if self.artist
+        self.artist.name
+     else
+       nil 
+     end
+  end
+
+end 
