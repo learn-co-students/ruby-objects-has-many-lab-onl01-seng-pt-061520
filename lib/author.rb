b9c05881d6e127a8c03d0cf404bc450b8a216cd0
Sydney Jenkins <sydjenkins168@gmail.com>
2020-07-17 21:22:11 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,39 @@
+require 'pry'
+class Author 
+  attr_accessor :name, :posts 
+  @@post_count = 0 
+  
+  def initialize(name)
+    @name=name
+  end 
+  
+  def posts 
+    Post.all.select do |post|
+      post.author == self
+    end 
+  end 
+  
+  def add_post(post)
+    post.author = self
+    @@post_count += 1 
+  end 
+  
+  def add_post_by_title(title)
+    new_post = Post.new(title)
+    add_post(new_post)
+  end 
+  
+  def self.post_count
+    Post.all.count 
+  end 
+    
+    
+    
+  
+end 
+  
+  
+  
+  
+  
+  
