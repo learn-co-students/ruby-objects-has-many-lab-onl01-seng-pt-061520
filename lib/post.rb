dc8593d1b6eef9f383bdf134845a7ef874602495
lizaetillman <lizaetillman@gmail.com>
2020-07-22 20:11:50 -0400

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,26 @@
+class Post # should belong to an author
+    
+    attr_accessor :title, :author
+
+    @@all = []
+
+    def initialize(title, author=nil)
+        @title = title
+        @author = author
+        @@all << self
+    end
+
+    def self.all
+        @@all
+     end
+
+           
+    def author_name
+        if self.author
+            self.author.name
+        else 
+            nil
+        end
+    end
+
+end
