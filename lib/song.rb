66d4f8614009c67137173de0f3d8b0e7def6dcde
Christopher Dutcher <cadutcher@icloud.com>
2020-07-30 23:13:13 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,31 @@
+class Song
+
+  attr_accessor :name, :artist
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def add_artist(artist)
+    artist.song = self
+    end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+      @@all
+  end
+
+  def artist_name
+    if self.artist
+      self.artist.name
+    else
+      nil
+    end
+  end
+
+end
